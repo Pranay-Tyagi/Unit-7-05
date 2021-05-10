f3a4520f1ef9511b5ff0e028e4525d45d73cfe20
PranayTyagi1 <>
2021-05-10 04:24:07 +0000

    tokens

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.querySelector('button').addEventListener('click', function () {
   const day = document.querySelector('.day').value
-  le age = document.querySelector('.age').value
+  let age = document.querySelector('.age').value
   age = parseInt(age)
   if ((day === 'wednesday' || day === 'thursday' || day === 'friday' || day === 'tuesday' || day === 'monday') && age < 18) {
     alert('time for school')
